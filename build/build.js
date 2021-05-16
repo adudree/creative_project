1d7d5fac8afdda62c5359c549d310a507bf3f0e0
Audrey <adudree@gmail.com>
2021-05-16 19:15:55 +0200

    add color

--- build/build.js
+++ build/build.js
@@ -1,85 +1,176 @@
-var player = new mm.Player();
-var isDisplayed = false;
-var TWINKLE_TWINKLE = {
-    notes: [
-        { pitch: 60, startTime: 0.0, endTime: 0.5 },
-        { pitch: 60, startTime: 0.5, endTime: 1.0 },
-        { pitch: 67, startTime: 1.0, endTime: 1.5 },
-        { pitch: 67, startTime: 1.5, endTime: 2.0 },
-        { pitch: 69, startTime: 2.0, endTime: 2.5 },
-        { pitch: 69, startTime: 2.5, endTime: 3.0 },
-        { pitch: 67, startTime: 3.0, endTime: 4.0 },
-        { pitch: 65, startTime: 4.0, endTime: 4.5 },
-        { pitch: 65, startTime: 4.5, endTime: 5.0 },
-        { pitch: 64, startTime: 5.0, endTime: 5.5 },
-        { pitch: 64, startTime: 5.5, endTime: 6.0 },
-        { pitch: 62, startTime: 6.0, endTime: 6.5 },
-        { pitch: 62, startTime: 6.5, endTime: 7.0 },
-        { pitch: 60, startTime: 7.0, endTime: 8.0 },
-    ],
-    totalTime: 8
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
 };
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+var _this = this;
+var gui = new dat.GUI();
+var params = {
+    dureeNote: 0.5,
+    steps_RNN: 12,
+    temperature_RNN: 1.1
+};
+gui.add(params, "dureeNote", 0.5, 4, 0.5);
+gui.add(params, "steps_RNN", 4, 24, 4);
+gui.add(params, "temperature_RNN", 0.1, 1.5, 0.1);
+var checkPoint = 'https://storage.googleapis.com/magentadata/js/checkpoints/music_rnn/';
+var modele = 'melody_rnn';
+var modeleRNN = checkPoint.concat(modele);
+var melodyRNN = new mm.MusicRNN(modeleRNN);
+var ownComp = {
+    notes: [],
+    totalTime: 0.
+};
+var keyboardValue = {
+    "q": 60,
+    "z": 61,
+    "s": 62,
+    "e": 63,
+    "d": 64,
+    "f": 65,
+    "t": 66,
+    "g": 67,
+    "y": 68,
+    "h": 69,
+    "u": 70,
+    "j": 71,
+    "k": 72
+};
+var keyboardColor = {
+    "q": "#d7323e",
+    "z": "#dd704a",
+    "s": "#e0a952",
+    "e": "#f4d45c",
+    "d": "#ffe868",
+    "f": "#e5e252",
+    "t": "#c1e562",
+    "g": "#9ad570",
+    "y": "#5fc771",
+    "h": "#589c5d",
+    "u": "#44746c",
+    "j": "#156b76",
+    "k": "#1e5598"
+};
+var rnn_steps = params.steps_RNN;
+var rnn_temp = params.temperature_RNN;
+var dureeNote = params.dureeNote;
+var piano = "https://storage.googleapis.com/magentadata/js/soundfonts/salamander";
+var playerPiano = new mm.SoundFontPlayer(piano);
+function draw() {
+    rnn_steps = params.steps_RNN;
+    rnn_temp = params.temperature_RNN;
+    dureeNote = params.dureeNote;
+}
+var suiteMelodie = function () { return __awaiter(_this, void 0, void 0, function () {
+    var quantizedSequence, impro, musique_1, playMelodieIa, stopMelodyIA, error_1;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0:
+                _a.trys.push([0, 4, , 5]);
+                return [4, melodyRNN.initialize()];
+            case 1:
+                _a.sent();
+                quantizedSequence = mm.sequences.quantizeNoteSequence(ownComp, 1);
+                return [4, melodyRNN.continueSequence(quantizedSequence, rnn_steps, rnn_temp)];
+            case 2:
+                impro = _a.sent();
+                impro.totalTime = 1;
+                return [4, mm.sequences.concatenate([quantizedSequence, impro])];
+            case 3:
+                musique_1 = _a.sent();
+                displayPlayer(musique_1, 'canvasMelodyIA');
+                playMelodieIa = document.getElementById('playIA').onclick = function () {
+                    playerPiano.loadSamples(musique_1).then(function () { playerPiano.start(musique_1); });
+                };
+                stopMelodyIA = document.getElementById('stopIA').onclick = function () {
+                    playerPiano.stop(musique_1);
+                };
+                document.getElementById('melodyIA').style.display = 'block';
+                displayPlayer(musique_1, 'canvasMelodyIA');
+                isDisplayed = true;
+                return [3, 5];
+            case 4:
+                error_1 = _a.sent();
+                console.log(error_1);
+                return [3, 5];
+            case 5: return [2];
+        }
+    });
+}); };
+var isDisplayed = false;
+var compEncours = false;
+var finComp = false;
+var playMelody = document.getElementById('play').onclick = function () {
+    if (finComp == true) {
+        playerPiano.loadSamples(ownComp).then(function () { playerPiano.start(ownComp); });
+    }
+    var stopMelody = document.getElementById('stop').onclick = function () {
+        if (finComp == true) {
+            playerPiano.stop(ownComp);
+        }
+    };
+};
+var createMelodyIA = document.getElementById('calculateMelodyIA').onclick = function () {
+    if (isDisplayed)
+        document.getElementById('melodyIA').style.display = 'none';
+    suiteMelodie();
+    isDisplayed = !isDisplayed;
+};
+var afficherMelodyIA = document.getElementById('melodyIA');
+afficherMelodyIA.style.display = "none";
+var afficherVizComp = document.getElementById("vizComposition");
+afficherVizComp.style.display = "none";
+document.getElementById("newMelody").onclick = function () { window.location.reload(); };
+document.getElementById("newMelody2").onclick = function () { window.location.reload(); };
 function setup() {
     p6_CreateCanvas();
     textAlign(LEFT);
-    createElement('h1', "Melod'IMAC");
-    createElement('p', "You would like to compose your own melody and you have started it, but you don't know how to continue it? Melod'IMAC is here for you!");
-    createElement('h2', "Let's compose!");
-    createElement('p', "Compose your own melody (8 notes) by playing with your keyboard : A = C (do), Z = D (ré), E = E (mi), R = F (fa), T = G (sol), Y = A (la), U = B (si), I = C (do)");
-    var canvas1 = createElement('canvas');
-    canvas1.id('melody');
-    displayPlayer('melody');
-    var playMelody = createButton("play");
-    playMelody.mousePressed(startMelody);
-    var stopMelody = createButton("stop");
-    stopMelody.mousePressed(endMelody);
-    createElement('h2', "Let's calculate!");
-    var calculateAI = createButton("CALCULATE THE END OF THE MELODY");
-    calculateAI.mousePressed(calculate);
 }
 function windowResized() {
     p6_ResizeCanvas();
 }
-var valueNote;
-function keyTyped() {
-    if (key === 'a') {
-        valueNote = 64;
-    }
-    else if (key === 'z') {
-        valueNote = 66;
-    }
-    else if (key === 'e') {
-        valueNote = 68;
-    }
-    else if (key === 'r') {
-        valueNote = 69;
-    }
-    else if (key === 't') {
-        valueNote = 71;
-    }
-    else if (key === 'y') {
-        valueNote = 73;
-    }
-    else if (key === 'u') {
-        valueNote = 75;
-    }
-    else if (key === 'i') {
-        valueNote = 76;
-    }
-}
-function startMelody() {
-    player.start(TWINKLE_TWINKLE);
-}
-function endMelody() {
-    player.stop(TWINKLE_TWINKLE);
-}
-function startAll() {
-    player.start(TWINKLE_TWINKLE);
-}
-function endAll() {
-    player.stop(TWINKLE_TWINKLE);
-}
-function displayPlayer(id) {
+document.getElementById("startComp").onclick = function () {
+    compEncours = true;
+};
+document.getElementById("stopComp").onclick = function () {
+    compEncours = false;
+    finComp = true;
+    displayPlayer(ownComp, 'melody');
+    console.log(ownComp);
+    afficherVizComp.style.display = "block";
+};
+function displayPlayer(musique, id) {
     var config = {
         noteHeight: 6,
         pixelsPerTimeStep: 30,
@@ -87,28 +178,57 @@ function displayPlayer(id) {
         noteRGB: '8, 41, 64',
         activeNoteRGB: '240, 84, 119',
     };
-    var viz = new mm.PianoRollCanvasVisualizer(TWINKLE_TWINKLE, document.getElementById(id), config);
-    new mm.Player(false, {
-        run: function (note) { return viz.redraw(note); },
-        stop: function () { console.log('done'); }
-    });
+    var viz = new mm.PianoRollCanvasVisualizer(musique, document.getElementById(id), config);
 }
-function calculate() {
-    if (!isDisplayed)
-        displayEnd();
+var noteMusique;
+var t1, t2;
+var playerNote = new mm.SoundFontPlayer(piano);
+function colorNote(key) {
+    document.getElementById(key).style.background = keyboardColor[key];
 }
-function displayEnd() {
-    var canvas2 = createElement('canvas');
-    canvas2.id('all');
-    displayPlayer('all');
-    createElement('p', "Congrats! The end of you melody have been created, let's listen to it ! If you don't like it, you can calculate a different ending by clicking on the previous button.");
-    var playAll = createButton("play");
-    playAll.mousePressed(startAll);
-    var stopAll = createButton("stop");
-    stopAll.mousePressed(endAll);
-    isDisplayed = true;
+function removeColor(key) {
+    document.getElementById(key).style.removeProperty('background');
 }
-function draw() { }
+function keyPressed() {
+    if (compEncours) {
+        t1 = millis();
+        if (key in keyboardValue) {
+            colorNote(key);
+            noteMusique = keyboardValue[key];
+            var totTime = ownComp["totalTime"];
+            var note = {
+                pitch: noteMusique,
+                startTime: totTime,
+                endTime: totTime += dureeNote
+            };
+            var noteUnique_1 = {
+                notes: [
+                    {
+                        pitch: noteMusique, startTime: 0., endTime: dureeNote
+                    }
+                ],
+                totalTime: dureeNote
+            };
+            playerNote.loadSamples(noteUnique_1).then(function () {
+                if (playerNote.isPlaying()) {
+                    playerNote.stop(noteUnique_1);
+                }
+                playerNote.start(noteUnique_1);
+            });
+            ownComp["notes"].push(note);
+            ownComp["totalTime"] += dureeNote;
+        }
+    }
+    return false;
+}
+function keyReleased() {
+    if (compEncours && key in keyboardValue) {
+        t2 = millis();
+        console.log("t2 :", t2);
+        removeColor(key);
+    }
+    return false;
+}
 var __MARGIN_SIZE = 0;
 function __desiredCanvasWidth() {
     return windowWidth - __MARGIN_SIZE * 2;
--- src/sketch.ts
+++ src/sketch.ts
@@ -2,54 +2,154 @@
 //    Initialization
 // -------------------
 
-// pour l'instant : sert d'exemple / de test 
-let TWINKLE_TWINKLE = {
-    notes: [
-      {pitch: 60, startTime: 0.0, endTime: 0.5},
-      {pitch: 60, startTime: 0.5, endTime: 1.0},
-      {pitch: 67, startTime: 1.0, endTime: 1.5},
-      {pitch: 67, startTime: 1.5, endTime: 2.0},
-      {pitch: 69, startTime: 2.0, endTime: 2.5},
-      {pitch: 69, startTime: 2.5, endTime: 3.0},
-      {pitch: 67, startTime: 3.0, endTime: 4.0},
-      {pitch: 65, startTime: 4.0, endTime: 4.5},
-      {pitch: 65, startTime: 4.5, endTime: 5.0},
-      {pitch: 64, startTime: 5.0, endTime: 5.5},
-      {pitch: 64, startTime: 5.5, endTime: 6.0},
-      {pitch: 62, startTime: 6.0, endTime: 6.5},
-      {pitch: 62, startTime: 6.5, endTime: 7.0},
-      {pitch: 60, startTime: 7.0, endTime: 8.0},  
-    ],
-    totalTime: 8
-};
+const gui = new dat.GUI()
+const params = {
+    dureeNote : 0.5,
+    steps_RNN : 12,
+    temperature_RNN : 1.1
+}
 
-let player = new mm.Player();
+gui.add(params, "dureeNote", 0.5, 4, 0.5)
+gui.add(params, "steps_RNN", 4, 24, 4)
+gui.add(params, "temperature_RNN", 0.1, 1.5, 0.1)
+
+// pour l'instant : sert d'exemple / de test 
+// Checkpoint : Adresse de tous les modèles de ML préentrainés de Magenta.js
+// CheckPoint+modele = URL contenant le modèle de ML que l'on désire  
+const checkPoint = 'https://storage.googleapis.com/magentadata/js/checkpoints/music_rnn/';
+const modele = 'melody_rnn' // Modèle qui permet de continuer une mélodie
+
+const modeleRNN = checkPoint.concat(modele); //On va chercher le modèle que l'on désire
+const melodyRNN = new mm.MusicRNN(modeleRNN); //On charge ce modèle
+
+let ownComp = {
+    notes: [],
+    totalTime : 0.
+}
+
+let keyboardValue = {
+    "q" : 60,
+    "z" : 61,
+    "s" : 62,
+    "e" : 63,
+    "d" : 64,
+    "f" : 65,
+    "t" : 66,
+    "g" : 67,
+    "y" : 68,
+    "h" : 69,
+    "u" : 70,
+    "j" : 71,
+    "k" : 72
+}
+
+let keyboardColor = {
+    "q" : "#d7323e",
+    "z" : "#dd704a",
+    "s" : "#e0a952",
+    "e" : "#f4d45c",
+    "d" : "#ffe868",
+    "f" : "#e5e252",
+    "t" : "#c1e562",
+    "g" : "#9ad570",
+    "y" : "#5fc771",
+    "h" : "#589c5d",
+    "u" : "#44746c",
+    "j" : "#156b76",
+    "k" : "#1e5598"
+}
+
+// GUI PARAM //
+let rnn_steps = params.steps_RNN; //Nbre de steps que l'on veut ajouter à la musique originale 
+let rnn_temp = params.temperature_RNN; //A quel point on veut une séquence différente de l'originale (temp >1.5 ==> Suite de la séquence sera quasi random)
+let dureeNote = params.dureeNote;
+
+// -------------
+// INSTRUMENTS 
+// -------------
+
+const piano = "https://storage.googleapis.com/magentadata/js/soundfonts/salamander"
+let playerPiano = new mm.SoundFontPlayer(piano);
+
+// -------------------
+//       Drawing
+// -------------------
+function draw() {
+    rnn_steps = params.steps_RNN;
+    rnn_temp = params.temperature_RNN;
+    dureeNote = params.dureeNote;
+}
+
+// -------------------------------
+//   Construction de la mélodie
+// ------------------------------
+
+const suiteMelodie = async() => {
+    try {
+        await melodyRNN.initialize()
+        const quantizedSequence = mm.sequences.quantizeNoteSequence(ownComp, 1) // Wewe on quantize le truc jsp trop ce que ca veut dire
+        let impro = await melodyRNN.continueSequence(quantizedSequence, rnn_steps, rnn_temp)
+        impro.totalTime = 1 //Corriger un petit bug
+        const musique = await mm.sequences.concatenate([quantizedSequence, impro])
+
+        displayPlayer(musique, 'canvasMelodyIA');
+
+        let playMelodieIa = document.getElementById('playIA').onclick = function() {
+            playerPiano.loadSamples(musique).then(()=> 
+            { playerPiano.start(musique)}
+                )}
+        
+        let stopMelodyIA =document.getElementById('stopIA').onclick = function() {
+            playerPiano.stop(musique);}
+
+        document.getElementById('melodyIA').style.display= 'block';
+        displayPlayer(musique, 'canvasMelodyIA');   
+        isDisplayed = true;
+    
+    }
+    catch(error){
+        console.log(error);
+    }
+}
+
 let isDisplayed = false; // pour l'affichage de la fin de la page après calcul par l'IA 
+let compEncours = false;
+let finComp = false;
 
 let playMelody = document.getElementById('play').onclick = function() {
-    player.start(TWINKLE_TWINKLE);}
+    if (finComp==true)
+    {
+        playerPiano.loadSamples(ownComp).then(()=> 
+        { playerPiano.start(ownComp)}
+        )
+    }
 
 let stopMelody = document.getElementById('stop').onclick = function() {
-    player.stop(TWINKLE_TWINKLE);}
+    if (finComp==true)
+    {
+    playerPiano.stop(ownComp);}
+    }
+}
 
-let createMelodyIA = document.getElementById('createMelodyIA').onclick = function() {
-    if (!isDisplayed)
-        displayEnd();}
-
-let playMelodyIA = document.getElementById('playIA').onclick = function() {
-    player.start(TWINKLE_TWINKLE);}
-
-let stopMelodyIA =document.getElementById('stopIA').onclick = function() {
-    player.stop(TWINKLE_TWINKLE);}
+let createMelodyIA = document.getElementById('calculateMelodyIA').onclick = function() {
+    if (isDisplayed)
+        document.getElementById('melodyIA').style.display= 'none';
+    suiteMelodie();
+    isDisplayed=!isDisplayed;
+}
 
 let afficherMelodyIA = document.getElementById('melodyIA');
 afficherMelodyIA.style.display= "none";
 
+let afficherVizComp = document.getElementById("vizComposition");
+afficherVizComp.style.display = "none";
+
+document.getElementById("newMelody").onclick = function() {window.location.reload()}
+document.getElementById("newMelody2").onclick = function() {window.location.reload()}
+
 function setup() {
     p6_CreateCanvas();
     textAlign(LEFT);
-
-    displayPlayer('melody');
 }
 
 function windowResized() {
@@ -60,54 +160,19 @@ function windowResized() {
 //    Functions
 // -------------------
 
-let valueNote;
-//configuration du clavier (à revoir mdr) 
-function keyTyped(){
-    if (key === 'a') {
-        valueNote = 64;
-    }
-    else if (key === 'z') {
-        valueNote = 66;
-    }
-    else if (key === 'e') {
-        valueNote = 68;
-    }
-    else if (key === 'r') {
-        valueNote = 69;
-    }
-    else if (key === 't') {
-        valueNote = 71;
-    }
-    else if (key === 'y') {
-        valueNote = 73;
-    }
-    else if (key === 'u') {
-        valueNote = 75;
-    }
-    else if (key === 'i') {
-        valueNote = 76;
-    }
-}
+document.getElementById("startComp").onclick = function(){
+    compEncours = true;
+} 
 
-// j'ai pas réussi à passer les mélodies en paramètre, vive le hardcoding
-/*function startMelody() {
-    player.start(TWINKLE_TWINKLE);
-}
+document.getElementById("stopComp").onclick = function(){
+    compEncours = false;
+    finComp = true;
+    displayPlayer(ownComp, 'melody'); // Display canvas whose id is melody
+    console.log(ownComp);
+    afficherVizComp.style.display = "block";
+} 
 
-function stopMelody() {
-    player.stop(TWINKLE_TWINKLE);
-}
-
-function startAll() {
-    player.start(TWINKLE_TWINKLE);
-}
-
-function endAll() {
-    player.stop(TWINKLE_TWINKLE);
-}
-*/
-
-function displayPlayer(id:string){
+function displayPlayer(musique, id:string){
     let config = {
         noteHeight: 6,
         pixelsPerTimeStep: 30,  // like a note width
@@ -116,24 +181,62 @@ function displayPlayer(id:string){
         activeNoteRGB: '240, 84, 119',
       };
 
-    let viz = new mm.PianoRollCanvasVisualizer(TWINKLE_TWINKLE, document.getElementById(id), config);   
-    new mm.Player(false, {
-        run: (note) => viz.redraw(note),
-        stop: () => {console.log('done');}
-      });
+    let viz = new mm.PianoRollCanvasVisualizer(musique, document.getElementById(id), config);   
 }
 
+let noteMusique;
+let t1, t2;
 
-function displayEnd() {
+let playerNote = new mm.SoundFontPlayer(piano);
 
-    /* insert visualize here */
-    document.getElementById('melodyIA').style.display= 'block';
-    displayPlayer('canvasMelodyIA');   
-    isDisplayed = true;
+function colorNote(key) {
+    document.getElementById(key).style.background = keyboardColor[key];    
+}
+function removeColor(key) {
+    document.getElementById(key).style.removeProperty('background');
 }
 
-// -------------------
-//       Drawing
-// -------------------
+function keyPressed(){ //Renvoie la valeur en pitch
+    if (compEncours){
+        t1 = millis();
+        if (key in keyboardValue){
+            colorNote(key);
 
-function draw() {}
+            noteMusique = keyboardValue[key];
+            let totTime = ownComp["totalTime"];
+            let note = {
+                pitch : noteMusique,
+                startTime : totTime,
+                endTime : totTime+=dureeNote
+                };
+
+            let noteUnique = {
+                notes : [
+                {  //Faire jouer la note en même temps
+                pitch : noteMusique, startTime : 0., endTime : dureeNote}
+            ],
+                totalTime: dureeNote
+            };
+
+            playerNote.loadSamples(noteUnique).then(()=> {   
+            if (playerNote.isPlaying()){ //Pour jouer plusieurs notes en même temps
+                playerNote.stop(noteUnique);
+            }
+            playerNote.start(noteUnique);
+            })
+
+            ownComp["notes"].push(note);
+            ownComp["totalTime"]+=dureeNote;
+            }
+        }
+        return false;
+}
+
+function keyReleased(){
+    if (compEncours && key in keyboardValue){
+        t2=millis()
+        console.log("t2 :", t2);
+        removeColor(key);
+    }
+    return false;
+}
